7705478d2869a709fc04024ffd7322bd6fbf74c6
TonyQ2k3 <ntbquoc2015@gmail.com>
2024-06-24 14:45:31 +0700

    Update

--- terraform/all/main.tf
+++ terraform/all/main.tf
@@ -18,12 +18,14 @@ locals {
   node_group_name = "test-nodes"
   prod_cluster_name = "production-environment"
   prod_node_group_name = "production-nodes"
+  stage_cluster_name = "staging-environment"
+  stage_node_group_name = "staging-nodes"
   vpc_id = "vpc-03312ce83615cf80a"
   subnet_ids = ["subnet-0930513f1b4fe91fc", "subnet-02732b19faaaf68e2"]
   iam_role_arn = "arn:aws:iam::624899937274:role/LabRole"
 }
 
-# EKS Cluster provisioning
+# EKS Test Cluster provisioning
 resource "aws_eks_cluster" "test_cluster" {
   name     = local.cluster_name
   role_arn = local.iam_role_arn
@@ -43,9 +45,9 @@ resource "aws_eks_node_group" "test_node_group" {
   subnet_ids       = local.subnet_ids
 
   scaling_config {
-    desired_size = 4
-    max_size     = 4
-    min_size     = 4
+    desired_size = 3
+    max_size     = 3
+    min_size     = 3
   }
 
   update_config {
@@ -122,9 +124,9 @@ resource "aws_eks_node_group" "prod_node_group" {
   subnet_ids       = local.subnet_ids
 
   scaling_config {
-    desired_size = 4
-    max_size     = 4
-    min_size     = 4
+    desired_size = 3
+    max_size     = 3
+    min_size     = 3
   }
 
   update_config {
@@ -161,4 +163,75 @@ resource "aws_eks_addon" "eks-pod-identity-agent2" {
   addon_name = "eks-pod-identity-agent"
   addon_version = "v1.2.0-eksbuild.1"
   depends_on = [ aws_eks_cluster.production_cluster ]
+}
+
+
+
+resource "aws_eks_cluster" "staging_cluster" {
+  name     = local.stage_cluster_name
+  role_arn = local.iam_role_arn
+
+  vpc_config {
+    subnet_ids = local.subnet_ids
+  }
+
+  version = "1.29" 
+}
+
+# EKS Node Group
+resource "aws_eks_node_group" "staging_node_group" {
+  cluster_name    = local.stage_cluster_name
+  node_group_name = local.stage_node_group_name
+  node_role_arn    = local.iam_role_arn
+  subnet_ids       = local.subnet_ids
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 3
+    min_size     = 3
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [ aws_eks_cluster.staging_cluster ]
+}
+
+output "endpoint1" {
+  value = aws_eks_cluster.staging_cluster.endpoint
+}
+
+output "kubeconfig-certificate-authority-data1" {
+  value = aws_eks_cluster.staging_cluster.certificate_authority[0].data
+}
+
+
+# EKS Add-ons
+resource "aws_eks_addon" "coredns3" {
+  cluster_name = local.stage_cluster_name
+  addon_name = "coredns"
+  addon_version = "v1.11.1-eksbuild.4"
+  depends_on = [ aws_eks_cluster.staging_cluster, aws_eks_node_group.staging_node_group ]
+}
+
+resource "aws_eks_addon" "kube-proxy3" {
+  cluster_name = local.stage_cluster_name
+  addon_name = "kube-proxy"
+  addon_version = "v1.29.0-eksbuild.1"
+  depends_on = [ aws_eks_cluster.staging_cluster ]
+}
+
+resource "aws_eks_addon" "vpc-cni3" {
+  cluster_name = local.stage_cluster_name
+  addon_name = "vpc-cni"
+  addon_version = "v1.16.0-eksbuild.1"
+  depends_on = [ aws_eks_cluster.staging_cluster]
+}
+
+resource "aws_eks_addon" "eks-pod-identity-agent3" {
+  cluster_name = local.stage_cluster_name
+  addon_name = "eks-pod-identity-agent"
+  addon_version = "v1.2.0-eksbuild.1"
+  depends_on = [ aws_eks_cluster.staging_cluster ]
 }
